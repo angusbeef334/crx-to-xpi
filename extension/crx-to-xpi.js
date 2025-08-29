dd91e23ebfe51d67951c898fa0e67a4eff2b1cda
Benjamin Yang <benjamin.yang12@outlook.com>
2025-08-29 16:54:10 +1000

    add info to page when installing

--- extension/crx-to-xpi.js
+++ extension/crx-to-xpi.js
@@ -1,21 +1,23 @@
-function callback(ret) {
-  window.open('http://localhost:8000');
-}
-
 function add() {
+  document.getElementById('crx-to-xpi-install').innerText = "Installing...";
   const split = location.href.split('/');
   const id = split[split.length - 1].split('?')[0];
   browser.runtime.sendMessage({ action: "install", message: id })
-    .then(callback)
+    .then(() => {
+      opened = window.open('http://localhost:8000');
+      document.getElementById('crx-to-xpi-install').innerText = "Add to Firefox";
+    })
     .catch((error) => {
       console.log(`Failed to install extension: ${error}`);
       alert(`Failed to install extension: ${error}`);
+      document.getElementById('crx-to-xpi-install').innerText = "Add to Firefox";
     });
 }
 
 function replace() {
   const btn = document.getElementsByClassName('UywwFc-LgbsSe UywwFc-LgbsSe-OWXEXe-dgl2Hf UywwFc-StrnGf-YYd4I-VtOx3e UywwFc-kSE8rc-FoKg4d-sLO9V-YoZ4jf')[0];
   const newbtn = document.createElement("button");
+  newbtn.id = "crx-to-xpi-install";
   newbtn.textContent = "Add to Firefox";
   newbtn.addEventListener('click', () => add());
   if (btn && newbtn) btn.replaceWith(newbtn);
