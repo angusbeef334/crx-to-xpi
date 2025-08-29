1e89ebf38dfd4590e42ca74775e97d46b878fee2
Benjamin Yang <benjamin.yang12@outlook.com>
2025-08-29 16:21:50 +1000

    make button replace interval

--- extension/crx-to-xpi.js
+++ extension/crx-to-xpi.js
@@ -3,14 +3,17 @@ function callback(ret) {
 }
 
 function add() {
-  //browser.management.install("/Users/benjamin/crx_to_xpi/script/out.xpi"); 
   const split = location.href.split('/');
   const id = split[split.length - 1].split('?')[0];
   browser.runtime.sendMessage({ action: "install", message: id }).then(callback);
 }
 
-const btn = document.getElementsByClassName('UywwFc-LgbsSe UywwFc-LgbsSe-OWXEXe-dgl2Hf UywwFc-StrnGf-YYd4I-VtOx3e UywwFc-kSE8rc-FoKg4d-sLO9V-YoZ4jf')[0];
-const newbtn = document.createElement("button");
-newbtn.textContent = "Add to Firefox";
-newbtn.addEventListener('click', () => add());
-btn.replaceWith(newbtn);
+function replace() {
+  const btn = document.getElementsByClassName('UywwFc-LgbsSe UywwFc-LgbsSe-OWXEXe-dgl2Hf UywwFc-StrnGf-YYd4I-VtOx3e UywwFc-kSE8rc-FoKg4d-sLO9V-YoZ4jf')[0];
+  const newbtn = document.createElement("button");
+  newbtn.textContent = "Add to Firefox";
+  newbtn.addEventListener('click', () => add());
+  btn.replaceWith(newbtn);
+}
+
+setInterval(replace, 1000);
